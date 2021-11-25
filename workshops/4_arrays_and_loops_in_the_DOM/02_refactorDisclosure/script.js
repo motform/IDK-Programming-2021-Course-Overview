1a662eb96c885347c081284b5be314e15be9d17a
motform <love.lagerkvist@protonmail.com>
2021-11-25 15:45:47 +0100

    Make WS4 exercises

--- workshops/4_arrays_and_loops_in_the_DOM/02_refactorDisclosure/script.js
+++ workshops/4_arrays_and_loops_in_the_DOM/02_refactorDisclosure/script.js
@@ -20,23 +20,45 @@
  * 
  */
 
-const disclosures = document.querySelectorAll(".disclosure");
+const ___ = null; // a hack to get rid of the pesky "___ is undefined" warnings, you can just ignore it
 
-for (const disclosure of disclosures) {
-    const header = disclosure.querySelector(".disclosure-header");
-    const chevronUp = disclosure.querySelector(".chevron-up");
-    const chevronDown = disclosure.querySelector(".chevron-down");
-    const panel = disclosure.querySelector(".disclosure-panel");
+// Use querySelectorAll to get all elements of the class ".disclosure"
+const disclosures = ___;
 
-    header.addEventListener("pointerdown", (event) => {
-        if (panel.classList.contains("hide")) {
-            panel.classList.remove("hide");
-            chevronDown.classList.add("hide");
-            chevronUp.classList.remove("hide");
-        } else {
-            panel.classList.add("hide");
-            chevronDown.classList.remove("hide");
-            chevronUp.classList.add("hide");
-        }
-    });
+// Loop over `disclosures`, naming the loop variable `disclosure` (notice singular vs. plural form)
+for (const ___ of ___) {
+  // We have seen this part so many times by now that we don't really want to do it again
+  const header = disclosure.querySelector(".disclosure-header");
+  const chevronUp = disclosure.querySelector(".chevron-up");
+  const chevronDown = disclosure.querySelector(".chevron-down");
+  const panel = disclosure.querySelector(".disclosure-panel");
+
+  // add an event listener to header for "pointerdown"
+  ___.addEventListener(___, (event) => {
+
+    // check if the classList of the panel contains the hide class
+    if (panel.classList.contains(___)) {
+
+      // if so, remove the "hide" class from the panel
+      panel.classList.remove(___);
+
+      // toggle the chevrons by adding the "hide" class to chevronDown
+      chevronDown.___;
+
+      // removing the "hide" class from the chevronUp to show it 
+      chevronUp.___;
+
+      
+    } else { // If we don't contain the class, i.e. we are showing, we want to hide the panel
+
+      // in this case, we want to add the "hide" class to the panel
+      panel.___;
+
+      // make sure that the chevron points down by removing the "hide" class on chevronDown
+      chevronDown.___;
+
+      // in doing this, we of course have to add "hide" to chevronUp
+      chevronUp.___;
+    }
+  });
 }
--- workshops/4_arrays_and_loops_in_the_DOM/03_tabs/script.js
+++ workshops/4_arrays_and_loops_in_the_DOM/03_tabs/script.js
@@ -31,72 +31,131 @@
   * 
  */
 
+
+const ___ =  null; // a small hack to avoid syntax errors from uncommented `___`
+
 const tabTriggers = document.querySelector("#tab-triggers");
 const tabPanels = document.querySelector("#tab-panels");
 
+// First, we make a function called `toggleTab`, this will be used
+// as a callback function that fires when the user clicks on one of
+// the triggers in the tab bar
 const toggleTab = (event) => {
-    const triggers = document.querySelectorAll(".tab-trigger");
-    const clickedTabTrigger = event.target;
+  /* select all elements with the class ".tab-trigger" */
+  // const triggers = document.querySelectorAll("___"); 
 
-    // Highlight the trigger ther user clicked
-    for (const trigger of triggers) {
-        if (trigger === clickedTabTrigger) {
-            trigger.classList.add("trigger-focused");
-        } else {
-            trigger.classList.remove("trigger-focused");
-        }
+  /* get the tab we have clicked by looking at the `target` of the event */
+  // const clickedTabTrigger = ___;
+
+  // Highlight the trigger ther user clicked
+
+  /* loop over `triggers` with the loop varible `trigger` */
+  for (const ___ of ___) {
+    /* check if the trigger is the one the user clicked */
+    if (___ === ___) {
+      /* if that is the case, add the class`trigger-focused` */
+      trigger.classList.add(___);
+    } else {
+      /* if not, we don't want it to have the `trigger-focused` class */
+      trigger.classList.remove(___);
     }
+  }
 
-    // Show the corresponding panel
-    const panels = document.querySelectorAll(".tab-panel");
-    const tabPanelId = clickedTabTrigger.getAttribute("tab-panel-id");
+  // Now we need to show the corresponding panel! 
 
-    for (const panel of panels) {
-        console.log(panel.id, tabPanelId)
-        if (panel.id === tabPanelId) {
-            panel.classList.remove("hide");
-        } else {
-            panel.classList.add("hide");
-        }
+  /* first, get the panels by selecting all elements with the class ".tab-panel" */
+  const panels = document.querySelectorAll(___);
+
+  /* second, we need to get the "tab-panel-id" from the trigger, if you are confused by this, scroll down first. */
+  const tabPanelId = clickedTabTrigger.getAttribute("tab-panel-id");
+
+
+  /* Loop over `panels` with the loop variable `panel`  */
+  for (const ___ of ___) {
+
+    /* If the panel has the id that matches the tabPanelId that was in our triggers attribute, remove the css class "hide" */
+    if (panel.id === tabPanelId) {
+      panel.classList.remove(___);
+    } else {
+      /* otherwise, we obviously want to hide it */
+      panel.classList.add(___);
     }
+  }
+} // end of toggleTab
+
+
+// A small helper function that makes and returns a "panel" element 
+
+/* the function should take one input argument called `text` */
+const makePanel = (___) => {
+  /* create a "section" element */
+  const panel = document.createElement(___);
+
+  /* add the classes "tab-panel" & "hide"*/
+  panel.classList.add(___, ____);
+
+  /* append the text that we get as a function input */
+  panel.append(___);
+
+  /* return the panel*/
+  return ___;
 }
 
-const makePanel = (text) => {
-    const panel = document.createElement("section");
-    panel.classList.add("tab-panel", "hide");
-    panel.append(text);
-    return panel;
-}
 
+
+// A helper function that makes a tab, adds the event listeners and mounts it in the DOM
+
+/* Nothing to fill in here, but note how we are getting quite a lot of input,
+   we need an id to match trigger <-> panel, a label for the trigger, the panel (an html element)
+   and a boolean of wether or this tab is the default focus. */
 const makeTab = (tabId, label, panel, isDefaultFocus) => {
-    const trigger = document.createElement("li");
-    trigger.classList.add("tab-trigger");
-    trigger.append(label);
-    trigger.addEventListener("pointerdown", toggleTab);
-    trigger.setAttribute("tab-panel-id", tabId);
 
-    panel.id = tabId;
+  /* Make a new li */
+  const trigger = document.createElement(___);
+  /* Add the class "tab-trigger" */
+  trigger.classList.add(___);
 
-    if (isDefaultFocus) {
-        trigger.classList.add("trigger-focused");
-        panel.classList.remove("hide");
-    }
+  /* Append the function input `label` */
+  trigger.append(___);
 
-    tabTriggers.append(trigger);
-    tabPanels.append(panel);
+  /* Register the function `toggleTab` (see above) as an envent listener for "pointerdown" */
+  trigger.addEventListener("pointerdown", ___);
+
+  /* Here, we add the attribute "tab-panel-id" and set it to the value of our funciton input `tabId`.
+     Before: <li>Label</li>
+     After: <li tab-panel-id="tabId">Label</li>
+     We are allowed to add any arbitrary attribute to an element. */
+  trigger.setAttribute("tab-panel-id", tabId);
+
+  /* Make sure we also set the id of the panel to match the attribute we added to the trigger */
+  panel.id = tabId;
+
+  /* Check if we are the default focus */
+  if (___) {
+    /* If so, we add some classes and remove the "hide" class */
+    trigger.classList.add("trigger-focused");
+    panel.classList.remove("hide");
+  }
+
+  /* Finally, append the trigger to the list of tab triggers */
+  tabTriggers.append(___);
+
+  /* And the panel to the section with the panels */
+  tabPanels.append(___);
 }
 
 makeTab(
-    1,
-    "Account",
-    makePanel("To make changes to your account, please confirm the EULA."),
-    true
+  1,           // The tab id
+  "Account",   // The label of the tab/trigger
+  makePanel("To make changes to your account, please confirm the EULA."), // The panel with it's content
+  true         // Wether or not it is the tab selected by default
 );
 
 makeTab(
-    2,
-    "About",
-    makePanel("TABS Inc. are the premier tab producers of the Northen Midwest."),
-    false
+  2,         // The tab id
+  "About",   // The label of the tab/trigger
+  makePanel("TABS Inc. are the premier tab producers of the Northen Midwest."),
+  false      // Wether or not it is the tab selected by default
 );
 
+// Well done! This was a complicated one, so it is OK if we only get parts of it.
